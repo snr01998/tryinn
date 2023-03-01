1f5839f85fc5255945204507d8953a6973f90653
shreyasnrao <raoshreyas037@gmail.com>
2023-03-01 10:43:24 +0530

    loginform

--- index.php
+++ index.php
@@ -1,2 +1,52 @@
 <?php
 echo "dsfsd"; ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="uikit.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.3/dist/css/uikit.min.css" />
+
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div form="uk-section uk-container">
+            <div class="uk-grid uk-child-width-l-3@s uk-child-width-l-l" uk-grid="">
+        <form class="uk-form-stacked js-login">
+    <div class="uk-margin ">
+        <label class="uk-form-label" for="form-stacked-text">email</label>
+        <div class="uk-form-controls">…
+            <input class="uk-input" id="form-stacked-text" type="email" placeholder="emails" required='required'/>
+        </div>
+    </div>
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-text">password</label>
+        <div class="uk-form-controls">
+        <input class="uk-input" id="form-stacked-text" type="password" placeholder="password" required='required'/>
+        </div>
+    </div>
+    
+    <div class="uk-margin">
+        <button class="uk-button-default" type="submit">login</button>
+    </div>
+
+
+    </form>
+    </div>
+    </div>        
+        <script src="uikit.js" ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.3/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.3/dist/js/uikit-icons.min.js"></script>
+    </body>
+</html>
